7f657a22de3a94b7100bf8eb442544e2b37f0e7e
CryptoIceMLH <cryptoice@molonlabe.holdings>
2025-11-26 17:35:24 +0000

    Fix import error in views.py and clean up unused functions

--- views.py
+++ views.py
@@ -1,14 +1,10 @@
-# Description: Add your page endpoints here.
+# Web UI endpoints for BitSatCredit extension
 
-from http import HTTPStatus
-
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from lnbits.core.models import User
 from lnbits.decorators import check_user_exists
 from lnbits.helpers import template_renderer
-
-from .crud import get_server_by_id
 
 bitsatcredit_generic_router = APIRouter()
 
@@ -17,41 +13,11 @@ def bitsatcredit_renderer():
     return template_renderer(["bitsatcredit/templates"])
 
 
-#######################################
-##### ADD YOUR PAGE ENDPOINTS HERE ####
-#######################################
-
-
-# Backend admin page
-
-
 @bitsatcredit_generic_router.get("/", response_class=HTMLResponse)
 async def index(req: Request, user: User = Depends(check_user_exists)):
+    """Admin page for BitSatCredit extension"""
     return bitsatcredit_renderer().TemplateResponse(
         "bitsatcredit/index.html", {"request": req, "user": user.json()}
-    )
-
-
-# Frontend shareable page
-
-
-@bitsatcredit_generic_router.get("/{server_id}")
-async def server_public_page(req: Request, server_id: str):
-    server = await get_server_by_id(server_id)
-    if not server:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Server does not exist.")
-
-    public_page_name = getattr(server, "name_id", "")
-    public_page_description = getattr(server, "description_id", "")
-
-    return bitsatcredit_renderer().TemplateResponse(
-        "bitsatcredit/public_page.html",
-        {
-            "request": req,
-            "server_id": server_id,
-            "public_page_name": public_page_name,
-            "public_page_description": public_page_description,
-        },
     )
 
 
